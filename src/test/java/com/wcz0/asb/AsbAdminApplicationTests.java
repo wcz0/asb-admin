7b437aae33fa72409ebe34d8200f28180df7945a
wcz0 <wcz0@outlook.com>
2024-01-13 21:50:24 +0800

    init

---
+++ src/test/java/com/wcz0/asb/AsbAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcz0.asb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsbAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
